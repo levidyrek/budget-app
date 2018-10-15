377fdb6036e84c4ab6454ec8c1e2746d7ecf5a77
Levi Payne <levi.dyrek@gmail.com>
2018-10-15 19:29:54 -0400

    Updated Material UI again and fixed the functionality and styling of the top bar.

--- src/components/MonthPicker.js
+++ src/components/MonthPicker.js
@@ -4,25 +4,21 @@ import ActionToday from '@material-ui/icons/Today'
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
 import Button from '@material-ui/core/Button'
-// import white from '@material-ui/core/colors/white'
 
 
 export default class MonthPicker extends Component {
     render() {
         return (
             <div className="MonthPicker">
-                <Button>
-                    {/* <KeyboardArrowLeft color={white} /> */}
+                <Button className="button-left">
                     <KeyboardArrowLeft />
                 </Button>
-                {/* <ActionToday color={white} /> */}
                 <ActionToday />
                 <span className="dateText">
                     <span id="month">Jan</span>
                     <span id="year">2018</span>
                 </span>
-                <Button>
-                    {/* <KeyboardArrowRight color={white} /> */}
+                <Button className="button-right">
                     <KeyboardArrowRight />
                 </Button>
             </div>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,11 +2,9 @@ import React, {Component} from 'react'
 import './stylesheets/TopBar.css'
 import MonthPicker from '../containers/MonthPicker'
 import Button from '@material-ui/core/Button'
-import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import NavigationMoreVert from '@material-ui/icons/MoreVert'
 import MenuButton from '../containers/MenuButton'
-// import white from '@material-ui/core/colors/white'
 
 export default class TopBar extends Component {
 
@@ -22,11 +20,10 @@ export default class TopBar extends Component {
                     <li><MonthPicker/></li>
                 </ul>
                 <ul className="right">
-                    <Menu>
-                        {/* <Button><NavigationMoreVert color={white} /></Button> */}
-                        <Button><NavigationMoreVert /></Button>
-                        <MenuItem primaryText="Log Out" onClick={this.props.logOut} />
-                    </Menu>
+                    <Button><NavigationMoreVert /></Button>
+                    <MenuItem onClick={this.props.logOut}>
+                        Logout
+                    </MenuItem>
                 </ul>
             </div>
         )
