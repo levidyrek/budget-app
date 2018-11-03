cf72dba1edd8eb8579470e1f9377ae85080fb27e
Levi Payne <levi.dyrek@gmail.com>
2018-11-02 20:26:20 -0400

    Replaced current top bar menu buttons with a single "My Account" menu, currently containing a logout option. Will soon show the logged in user's username.

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,16 +2,30 @@ import React, {Component} from 'react'
 import './stylesheets/TopBar.css'
 import MonthPicker from '../containers/MonthPicker'
 import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
-import NavigationMoreVert from '@material-ui/icons/MoreVert'
 import MenuButton from '../containers/MenuButton'
 
 export default class TopBar extends Component {
 
+    state = {
+        anchorEl: null,
+    };
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
     render() {
+        const { anchorEl } = this.state;
+
         return (
-            <div className="TopBar">
-                <ul className="left">
+            <div className='TopBar'>
+                <ul className='left'>
                     <li>
                         {this.props.mobileMode &&
                             <MenuButton />
@@ -19,13 +33,24 @@ export default class TopBar extends Component {
                     </li>
                     <li><MonthPicker/></li>
                 </ul>
-                <ul className="right">
-                    <Button><NavigationMoreVert /></Button>
-                    <MenuItem onClick={this.props.logOut}>
-                        Logout
-                    </MenuItem>
+                <ul className='right'>
+                    <Button
+                        aria-owns={anchorEl ? 'user-menu' : null}
+                        aria-haspopup='true'
+                        onClick={this.handleClick}
+                    >
+                        My Account
+                    </Button>
+                    <Menu
+                        id='user-menu'
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={this.handleClose}
+                    >
+                        <MenuItem onClick={this.props.logOut}>Logout</MenuItem>
+                    </Menu>
                 </ul>
             </div>
         )
-    }
-}
+    };
+};
