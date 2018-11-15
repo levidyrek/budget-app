78af58456429ee019d328fd9255b3ce62380a08c
Levi Payne <levi.dyrek@gmail.com>
2018-11-14 19:05:36 -0500

    Made month picker switch displayed months when the next/previous arrow buttons are clicked. Removed react-month-picker.

--- src/components/MonthPicker.js
+++ src/components/MonthPicker.js
@@ -5,20 +5,75 @@ import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Button from '@material-ui/core/Button';
 
+const MONTHS = [
+  'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+  'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC',
+];
 
 export default class MonthPicker extends Component {
+  constructor(props) {
+    super(props);
+
+    const currentDate = new Date();
+    this.state = {
+      month: currentDate.getMonth() + 1,
+      year: currentDate.getFullYear(),
+    };
+  }
+
+  switchToPrevious = () => {
+    this.setState((state) => {
+      let month = state.month - 1;
+      let { year } = state;
+      if (month < 1) {
+        month = 12;
+        year -= 1;
+      }
+
+      return {
+        month,
+        year,
+      };
+    });
+  }
+
+  switchToNext = () => {
+    this.setState((state) => {
+      let month = state.month + 1;
+      let { year } = state;
+      if (month > 12) {
+        month = 1;
+        year += 1;
+      }
+
+      return {
+        month,
+        year,
+      };
+    });
+  }
+
   render() {
+    const { month, year } = this.state;
+    const monthText = MONTHS[month - 1];
+
     return (
       <div className="MonthPicker">
-        <Button className="button-left">
+        <Button
+          className="button-left"
+          onClick={this.switchToPrevious}
+        >
           <KeyboardArrowLeft />
         </Button>
         <ActionToday />
         <span className="dateText">
-          <span id="month">Jan</span>
-          <span id="year">2018</span>
+          <span id="month">{monthText}</span>
+          <span id="year">{year}</span>
         </span>
-        <Button className="button-right">
+        <Button
+          className="button-right"
+          onClick={this.switchToNext}
+        >
           <KeyboardArrowRight />
         </Button>
       </div>
