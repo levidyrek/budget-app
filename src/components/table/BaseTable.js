d62af61317d53428a71f057c83580a36726658ec
Levi Payne <levi.dyrek@gmail.com>
2018-12-08 13:05:09 -0500

    Added a row click callback to BaseTable to support editing rows.

--- src/components/table/BaseTable.js
+++ src/components/table/BaseTable.js
@@ -1,25 +1,51 @@
-import React, {Component} from 'react'
-import './stylesheets/BaseTable.css'
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import PropTypes from 'prop-types';
 
-// Import React Table
-import ReactTable from "react-table"
-import "react-table/react-table.css"
+import './stylesheets/BaseTable.css';
 
 
-export default class BaseTable extends Component {
-    onClickCell = (event) => {
+class BaseTable extends Component {
+  getTrProps = (state, rowInfo) => {
+    const { handleRowClick } = this.props;
 
-    }
+    return {
+      onClick: (e, handleOriginal) => {
+        if (handleRowClick) {
+          handleRowClick(rowInfo.original);
+        }
+        if (handleOriginal) {
+          handleOriginal();
+        }
+      },
+    };
+  }
 
-    render() {
-        return (
-            <ReactTable
-                data={this.props.rows}
-                columns={this.props.columns}
-                defaultPageSize={25}
-                minRows={25}
-                className="-striped -highlight"
-                />
-        )
-    }
+  render() {
+    const { columns, rows } = this.props;
+
+    return (
+      <ReactTable
+        data={rows}
+        columns={columns}
+        defaultPageSize={25}
+        minRows={25}
+        className="-striped -highlight"
+        getTrProps={this.getTrProps}
+      />
+    );
+  }
 }
+
+BaseTable.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleRowClick: PropTypes.func,
+  rows: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+BaseTable.defaultProps = {
+  handleRowClick: null,
+};
+
+export default BaseTable;
