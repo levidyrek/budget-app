31bbf41184cf560f9b62162dec52819e1d598762
Levi Payne <levi.dyrek@gmail.com>
2019-06-08 19:45:00 -0400

    Make API URL configurable.
    * Made use of a dotenv config file to set the API URL.
    * Create a sample dotenv file.

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,5 @@
 // API URLs
-export const BASE_API_URL = 'http://localhost:8000';
+export const BASE_API_URL = process.env.REACT_APP_API_URL;
 
 function getUrl(path) {
   return `${BASE_API_URL}${path}`;
